66ba3e015629eb77e39c1c3673de2d4dee885508
Mydeen <Mydeen1193@gmail.com>
2024-09-14 20:48:54 +0530

    add roll function

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,56 @@ import Die from "./component/Die"
 
 export default function App(){
   
-  const [dice , setDice] = React.useState(allDice())
+  const [dices , setDice] = React.useState(allDice())
+
+  function generateNewDice(){
+            return{
+              id : nanoid() ,
+              value : Math.ceil(Math.random()*6) ,
+              isHeld : false
+            }
+  }
 
   function allDice(){
     let randomdice = []
     for(let i = 0 ; i < 10 ; i++){
-      randomdice.push({
-        value : Math.ceil(Math.random()*6) ,
-        isHeld : false ,
-        id : nanoid()
-      })
+        randomdice.push( generateNewDice() )
     }
     return randomdice
+  
+  }
+  function rollDice(){
+    setDice(oldDice => oldDice.map(dice => {
+      return dice.isHeld ?
+                          dice :
+                          generateNewDice()
+
+    }))
   }
 
-  function roll(){
-    setDice(allDice)
+  function holdDice(id){
+    console.log(id)
+    setDice(oldDice => oldDice.map(dice => {
+      return dice.id === id ? 
+                            {...dice ,
+                               isHeld :!dice.isHeld} : 
+                                                    dice
+    }))
   }
-   let diceComponent = dice.map(dices => (
+
+   let diceComponent = dices.map(dice => (
    <Die  
-      key = {dices.id} 
-      values={dices.value}/>))
-  
+      key = {dice.id} 
+      values={dice.value}
+      isHeld = {dice.isHeld}
+      holdDice = { () => holdDice(dice.id)}
+    />))
   return(
     <div className="content">
       <div className="diceContainer">
           {diceComponent}
       </div>
-          <button className="roll-dice" onClick={roll} >Roll</button>
+          <button className="roll-dice" onClick={rollDice} >Roll</button>
     </div>
   )
 }
--- src/component/Die.jsx
+++ src/component/Die.jsx
@@ -1,7 +1,10 @@
 
 export default function die(props){
+        const styles = {
+                backgroundColor : props.isHeld ? "#59E391" : "white" 
+        }
    return( 
-        <div className="diceFace">
+        <div className="diceFace" style = {styles}  onClick={props.holdDice}>
         <div className="die1">{props.values}</div>
         </div>)
 }
